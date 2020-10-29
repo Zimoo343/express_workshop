7f17c9739058a1df5ae8ef99963abb807b66d8d8
David Ruiz <scdo46@hotmail.com>
2020-10-28 18:09:03 -0600

    User

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const morgan = require('morgan');
 const express = require('express');
 const app = express();
 const pokemon = require('./routes/pokemon');
+const user = require('./routes/user');
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -12,6 +13,7 @@ app.get("/", (req, res, next) => {
 });
 
 app.use("/pokemon", pokemon);
+app.use("/user", user);
 
 app.use((req, res, next) => {
     return res.status(404).json({ code: 404, message: "URL no encontreada" });
@@ -20,3 +22,4 @@ app.use((req, res, next) => {
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
 });
+
